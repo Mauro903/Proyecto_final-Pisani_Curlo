116d1ca168a685a4adecabb3642a0c6b2c8b47d5
Mauro Pisani Curlo <mauro903_7@hotmail.com>
2023-02-04 23:07:07 -0300

    !

--- hablemos_de_cine/hablemos_de_cine/urls.py
+++ hablemos_de_cine/hablemos_de_cine/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-from Pagina_principal.views import *
+from Pagina_principal.urls import *
 
 
 
